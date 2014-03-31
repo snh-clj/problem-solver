dbdba17b897f81955221662aaa9af2c14b3030fd
Tim McIver <tim@timmciver.com>
2014-03-31 01:45:13 -0400

    Tim McIver's attempt at a solution to the rollover time problem.  Uses clj-time.  Probably has bugs and misses corner cases.

---
+++ rollover-time/src/snh_problems/rollover_time/tjm.clj
@@ -0,0 +1,33 @@
+(ns snh-problems.rollover-time.tjm
+  (:require [clj-time.core :as t]))
+
+(defn normalize-schedule
+  "Creates a schedule with value for all fields; not just the ones the user
+supplied."
+  [schedule]
+  (merge {:year (range 2014 (Integer/MAX_VALUE))
+          :month (range 1 13)
+          :day (range 1 32)
+          :hour [0]
+          :min [0]
+          :sec [0]}
+         schedule))
+
+(defn all-events-from
+  "Returns a lazy seq of all the scheduled events after the given time for the
+given schedule."
+  [datetime schedule]
+  (let [schedule (normalize-schedule schedule)]
+    (drop-while #(t/after? datetime %)
+                (for [year (:year schedule)
+                      month (:month schedule)
+                      day (:day schedule)
+                      hour (:hour schedule)
+                      minute (:min schedule)
+                      second (:sec schedule)]
+                  (t/date-time year month day hour minute second)))))
+
+(defn next-event
+  "Returns the event that is to occur at or after the given time and schedule."
+  [datetime schedule]
+  (first (all-events-from datetime schedule)))
